59eb5633aa551329aa4679f75e54585815e478c5
Noe <noebeibei@gmail.com>
2020-07-23 21:33:24 +0000

    Add map to webpage
    -Map takes up the entire screen
    -Map begins zoomed in on Googleplex building
    -Map API key is hidden in separate file

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,3 +11,19 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+ 
+//Get API key from hidden file and use it to get the map
+var mykey = keys.MAPS_API_KEY;
+document.getElementById('mapUrl').src = mykey;
+ 
+//When the page loads, call createMap
+window.onload = function() {
+  createMap()
+};
+ 
+//Create a map zoomed in on Googleplex
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
