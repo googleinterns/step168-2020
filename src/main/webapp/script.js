9097813f4b24981bace8397e5d9757597fcc2929
Eyad Alsilimy <alsile@uw.edu>
2020-08-17 19:32:25 +0000

    Add animations to video maximize and minimize from added markers. Additionally: - fix bug causing search to crash if user zooms out far enough - removed console logs used for debugging - add helper methods to functions that were long - added markers to wherever users click or search - change enter key to search location and videos (not just videos, although maybe that was last commit)

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -22,6 +22,7 @@ document.getElementById('mapUrl').src = mykey;
 
 let player;
 let casesData;
+let overlay;
 let curLocationMarker;
 
 // When the page loads, call createMap
@@ -223,6 +224,14 @@ function createMap() {
       findWhatToSearch();
     }
   };
+  initOverlay();
+}
+
+// creates overlay of map
+function initOverlay() {
+  overlay = new google.maps.OverlayView();
+  overlay.draw = function() {};
+  overlay.setMap(map);
 }
 
 // searches 'COVID-19' if user doesn't specify search
@@ -283,6 +292,7 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+// places marker on map of clicked/searched location
 function placeMarker(map, curLocation) {
   if (typeof curLocationMarker === 'undefined') {
     curLocationMarker = new google.maps.Marker( {
@@ -291,6 +301,25 @@ function placeMarker(map, curLocation) {
     })
   } else {
     curLocationMarker.setPosition(curLocation);
+  }
+}
+
+// moves video starting position to current marker
+function setUpForMaximizeAnimation() {
+  if (typeof curLocationMarker !== 'undefined') {
+
+      const proj = overlay.getProjection();
+      const pos = curLocationMarker.getPosition();
+      const p = proj.fromLatLngToContainerPixel(pos);
+      const markerBubbleOffsetTop = -4;
+      const markerBubbleOffsetLeft = -.5;
+      // the distance between the marker position and the center of the bubble of the marker
+      const startLeft = p.x / window.innerWidth * 100 + markerBubbleOffsetLeft + '%';
+      const startTop = p.y / window.innerHeight * 100 + markerBubbleOffsetTop + '%';
+      $('#video-background').css('top', startTop);
+      $('#video-background').css('left', startLeft);
+      $('#video-background').css('width', '0%');
+      $('#video-background').css('height', '0%');
   }
 }
 
@@ -331,8 +360,10 @@ function getCoordsFromSearch(geocoder, map) {
   if (address !== '') {
     geocoder.geocode({address: address}, (results, status) => {
     if (status === 'OK') {
-      map.setCenter(results[0].geometry.location);
-      displayLatitudeLongitude(results[0].geometry.location.toJSON());
+      const foundLocation = results[0].geometry.location;
+      map.setCenter(foundLocation);
+      displayLatitudeLongitude(foundLocation.toJSON());
+      placeMarker(map, foundLocation.toJSON());
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
@@ -389,11 +420,13 @@ function gotoUserLocation(map) {
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
 }
 
+// makes videoplayer draggable
 $ ('#video-background').draggable( {
   cursor: 'move',
   iframeFix: true
 });
 
+// makes videoplayer resizable
 $(".resizable").resizable({
     start: function(event, ui) {
         ui.element.append($("<div/>", {
